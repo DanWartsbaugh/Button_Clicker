527e7a55324241c3cb409ac7e6ed2c8a520cd947
DanWartsbaugh <d.wartsbaugh@gmail.com>
2023-03-20 17:56:11 -0500

    Added comments

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+//Used to change the text of the Login button to Logout
 function logout(element){
     element.innerText="Logout"
 }
 
+//Used to remove the Add Definition button
 function hide(element){
     element.remove();
 }
